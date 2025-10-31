2c364fa5ba476572d62c46f712b0b6e439c6ab74
ditharaavindi <it23424432@my.sliit.lk>
2025-10-31 22:43:30 +0530

    update

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a9addd80bd3d5f25"; }
+/******/ 		__webpack_require__.h = function() { return "5a51de9d8f0b83a4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
